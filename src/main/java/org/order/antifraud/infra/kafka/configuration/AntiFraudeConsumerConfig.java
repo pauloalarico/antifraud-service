8c035d6859f8db6174786d86a419c1ada14784a3
pauloalarico <paulocraft20@gmail.com>
2026-02-23 01:55:55 -0300

    feature - add the concurrent kafka listener class, with the DefaultErrorHandler

--- src/main/java/org/order/antifraud/infra/kafka/configuration/AntiFraudeConsumerConfig.java
+++ src/main/java/org/order/antifraud/infra/kafka/configuration/AntiFraudeConsumerConfig.java
@@ -1,19 +1,25 @@
 package org.order.antifraud.infra.kafka.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.JacksonJsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 @ConfigurationProperties(prefix = "app.kafka")
+@Slf4j
 public class AntiFraudeConsumerConfig {
     private String consumerId;
     private String server;
@@ -30,5 +36,26 @@ public class AntiFraudeConsumerConfig {
         configs.put(JacksonJsonDeserializer.TRUSTED_PACKAGES, trustDeserializer);
         configs.put(JacksonJsonDeserializer.TYPE_MAPPINGS, mappingNewAntiFraud);
         return new DefaultKafkaConsumerFactory<>(configs);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
+        factory.setConsumerFactory(consumerFactory());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        factory.setConcurrency(3);
+        factory.setCommonErrorHandler(defaultErrorHandler());
+        return factory;
+    }
+
+    @Bean
+    public DefaultErrorHandler defaultErrorHandler() {
+        var backoff = new FixedBackOff(6000L, 3);
+        return new DefaultErrorHandler(
+                ((consumerRecord, e) ->
+                {
+                    log.error("Unable to process message: {}", e.getMessage());
+                }), backoff
+        );
     }
 }
