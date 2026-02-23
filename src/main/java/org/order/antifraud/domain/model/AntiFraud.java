572fea4078e03c34e290afc9fd9056e46a56b74a
pauloalarico <paulocraft20@gmail.com>
2026-02-23 01:55:19 -0300

    feature - add the risk classification and methods of payment

--- src/main/java/org/order/antifraud/domain/model/AntiFraud.java
+++ src/main/java/org/order/antifraud/domain/model/AntiFraud.java
@@ -1,5 +1,7 @@
 package org.order.antifraud.domain.model;
 
+import org.order.antifraud.domain.enums.MethodPayment;
+import org.order.antifraud.domain.enums.RiskFraud;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -12,5 +14,28 @@ public class AntiFraud {
     private String correlationId;
     private String consumerId;
     private BigDecimal amount;
+    private MethodPayment method;
+    private RiskFraud riskFraud;
 
+    public static AntiFraud create(String correlationId, String consumerId, BigDecimal amount, MethodPayment method) {
+        AntiFraud antiFraud = new AntiFraud();
+        antiFraud.correlationId = correlationId;
+        antiFraud.consumerId = consumerId;
+        antiFraud.amount = amount;
+        antiFraud.method = method;
+        antiFraud.riskFraud = RiskFraud.LOW;
+        return antiFraud;
+    }
+
+    public void highRisk() {
+        this.riskFraud = RiskFraud.HIGH;
+    }
+
+    public void lowRisk() {
+        this.riskFraud = RiskFraud.LOW;
+    }
+
+    public void setAsFraud() {
+        this.riskFraud = RiskFraud.FRAUD;
+    }
 }
