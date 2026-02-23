27fead91960f68d2ac6d5abdeb96e2e406bc6e68
pauloalarico <paulocraft20@gmail.com>
2026-02-23 01:56:53 -0300

    refactor - change the values of the dto from uuid to string

--- src/main/java/org/order/antifraud/application/dto/request/NewAntifraudProcessorCommand.java
+++ src/main/java/org/order/antifraud/application/dto/request/NewAntifraudProcessorCommand.java
@@ -1,14 +1,15 @@
 package org.order.antifraud.application.dto.request;
 
+import org.order.antifraud.domain.enums.MethodPayment;
+
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
-import java.util.UUID;
 
 public record NewAntifraudProcessorCommand (
         EventExternal eventPayment,
-        UUID paymentId,
-        UUID correlationId,
-        UUID costumerId,
+        String paymentId,
+        String correlationId,
+        String costumerId,
         BigDecimal amount,
         MethodPayment methodPayment,
         ZonedDateTime createdAt
