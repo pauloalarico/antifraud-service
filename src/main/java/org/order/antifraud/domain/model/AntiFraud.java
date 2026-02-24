373d758081a93e7a942cb5e85574deb48da6eb5b
pauloalarico <paulocraft20@gmail.com>
2026-02-24 00:29:15 -0300

    refactor: add getter and PaymentStatus for the entity

--- src/main/java/org/order/antifraud/domain/model/AntiFraud.java
+++ src/main/java/org/order/antifraud/domain/model/AntiFraud.java
@@ -1,6 +1,8 @@
 package org.order.antifraud.domain.model;
 
+import lombok.Getter;
 import org.order.antifraud.domain.enums.MethodPayment;
+import org.order.antifraud.domain.enums.PaymentStatus;
 import org.order.antifraud.domain.enums.RiskFraud;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +10,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.math.BigDecimal;
 
 @Document(collection = "${apps.}")
+@Getter
 public class AntiFraud {
     @Id
     private String id;
@@ -16,6 +19,7 @@ public class AntiFraud {
     private BigDecimal amount;
     private MethodPayment method;
     private RiskFraud riskFraud;
+    private PaymentStatus paymentStatus;
 
     public static AntiFraud create(String correlationId, String consumerId, BigDecimal amount, MethodPayment method) {
         AntiFraud antiFraud = new AntiFraud();
@@ -37,5 +41,17 @@ public class AntiFraud {
 
     public void setAsFraud() {
         this.riskFraud = RiskFraud.FRAUD;
+    }
+
+    public void mediumRisk() {
+        this.riskFraud = RiskFraud.MEDIUM;
+    }
+
+    public void approve() {
+        this.paymentStatus = PaymentStatus.APPROVED;
+    }
+
+    public void reprove() {
+        this.paymentStatus = PaymentStatus.REPROVED;
     }
 }
