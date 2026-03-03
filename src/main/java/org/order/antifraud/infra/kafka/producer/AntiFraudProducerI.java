ad3b70e2512203270cd9b7cab5f7ab85eb25f818
pauloalarico <paulocraft20@gmail.com>
2026-03-03 00:10:30 -0300

    chore: remove unused imports

--- src/main/java/org/order/antifraud/infra/kafka/producer/AntiFraudProducerI.java
+++ src/main/java/org/order/antifraud/infra/kafka/producer/AntiFraudProducerI.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.ExecutionException;
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
