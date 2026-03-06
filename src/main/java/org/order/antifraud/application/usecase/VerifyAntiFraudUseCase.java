22b276fd7f8377c87f685aec406a1643a182fa79
pauloalarico <paulocraft20@gmail.com>
2026-03-06 10:13:14 -0300

    refactor: adapt the usecase for the use of the new class

--- src/main/java/org/order/antifraud/application/usecase/VerifyAntiFraudUseCase.java
+++ src/main/java/org/order/antifraud/application/usecase/VerifyAntiFraudUseCase.java
@@ -2,66 +2,42 @@ package org.order.antifraud.application.usecase;
 
 import lombok.RequiredArgsConstructor;
 import org.order.antifraud.application.dto.request.NewAntifraudProcessorCommand;
-import org.order.antifraud.domain.enums.MethodPayment;
+import org.order.antifraud.application.dto.response.ResultAntiFraudService;
+import org.order.antifraud.domain.service.PaymentApprovalPolicy;
+import org.order.antifraud.domain.service.ScoreCalculator;
+import org.order.antifraud.domain.enums.RiskFraud;
 import org.order.antifraud.domain.model.AntiFraud;
+import org.order.antifraud.infra.mapper.AntiFraudMapper;
 import org.order.antifraud.infra.persistence.repository.AntiFraudMongoRepositoryI;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
 @Service
 @RequiredArgsConstructor
 public class VerifyAntiFraudUseCase {
-    private final static BigDecimal thousand = BigDecimal.valueOf(1000.00);
-    private final static BigDecimal hundred = BigDecimal.valueOf(100.00);
+
     private final AntiFraudMongoRepositoryI repository;
+    private final AntiFraudMapper mapper;
 
-    public AntiFraud verify(NewAntifraudProcessorCommand command) {
-        String correlationId = command.correlationId();
-        String consumerId = command.costumerId();
-        BigDecimal amount = command.amount();
-        MethodPayment method = command.methodPayment();
+    public ResultAntiFraudService verify(NewAntifraudProcessorCommand command) {
         ZonedDateTime dateTimeTransaction = command.createdAt();
-        var antiFraud = AntiFraud.create(correlationId, consumerId, amount, method);
+        var antiFraud = AntiFraud.create(
+                command.correlationId(),
+                command.costumerId(),
+                command.amount(),
+                command.methodPayment()
+        );
 
-        int score = 0;
+        RiskFraud riskFraud = ScoreCalculator.calculate(command.amount(), command.methodPayment(), dateTimeTransaction.getHour());
+        antiFraud.classifyRisk(riskFraud);
 
-        if(method == MethodPayment.CREDIT_CARD) {
-           score += 10;
+        if (PaymentApprovalPolicy.reproveTest(riskFraud, command.methodPayment())) {
+            antiFraud.reprove();
+        } else {
+            antiFraud.approve();
         }
-
-        if(method == MethodPayment.BILLING) {
-            score += 20;
-        }
-        var hourTime = dateTimeTransaction.getHour();
-        if(hourTime < 6 || hourTime > 22 ) {
-            score += 40;
-        }
-
-        if(amount.compareTo(thousand) > 0) {
-            score += 40;
-        }
-
-        if (amount.compareTo(hundred) > 0) {
-            score += 10;
-        }
-
-        if(score < 40) {
-            antiFraud.lowRisk();
-        }
-
-        if(score >= 40 && score < 65) {
-            antiFraud.mediumRisk();
-        }
-
-        if(score >= 65 && score < 90) {
-            antiFraud.highRisk();
-        }
-
-        if (score > 90) {
-            antiFraud.setAsFraud();
-        }
-        return repository.save(antiFraud);
+        repository.save(antiFraud);
+        return mapper.toResult(antiFraud);
     }
 }
