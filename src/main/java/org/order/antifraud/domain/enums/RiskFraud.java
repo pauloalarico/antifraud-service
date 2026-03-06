532cc73c0da7bd6ffe3e3af3f3e116241d069700
pauloalarico <paulocraft20@gmail.com>
2026-03-06 10:11:20 -0300

    refactor: change the creation of the Risk for the enum

--- src/main/java/org/order/antifraud/domain/enums/RiskFraud.java
+++ src/main/java/org/order/antifraud/domain/enums/RiskFraud.java
@@ -4,5 +4,22 @@ public enum RiskFraud {
     LOW,
     MEDIUM,
     HIGH,
-    FRAUD
+    FRAUD;
+
+
+    public static RiskFraud convertScoreToRisk(int score) {
+        if (score >= 90) {
+            return FRAUD;
+        }
+
+        if (score >= 65) {
+            return HIGH;
+        }
+
+        if (score >= 45) {
+            return MEDIUM;
+        }
+
+        return LOW;
+    }
 }
