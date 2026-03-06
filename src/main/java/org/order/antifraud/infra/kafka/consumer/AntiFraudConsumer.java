db0cf56d104de08fc226d72a7db799116aba3a59
pauloalarico <paulocraft20@gmail.com>
2026-03-06 10:13:37 -0300

    refactor: adapt the consume for use only the analyzepayment class

--- src/main/java/org/order/antifraud/infra/kafka/consumer/AntiFraudConsumer.java
+++ src/main/java/org/order/antifraud/infra/kafka/consumer/AntiFraudConsumer.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.order.antifraud.application.dto.request.NewAntifraudProcessorCommand;
 import org.order.antifraud.application.dto.response.ResultAntiFraudService;
-import org.order.antifraud.application.usecase.AnalyzePaymentUseCase;
 import org.order.antifraud.application.usecase.VerifyAntiFraudUseCase;
-import org.order.antifraud.domain.model.AntiFraud;
 import org.order.antifraud.infra.kafka.producer.AntiFraudProducer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -17,13 +15,11 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AntiFraudConsumer {
     private final VerifyAntiFraudUseCase verifyFraud;
-    private final AnalyzePaymentUseCase analyzePayment;
     private final AntiFraudProducer producer;
 
     @KafkaListener(topics = "${app.kafka.newPaymentTopic}")
     public void execute(NewAntifraudProcessorCommand command, Acknowledgment acknowledgment) {
-        AntiFraud antiFraud = verifyFraud.verify(command);
-        ResultAntiFraudService result = analyzePayment.analyze(antiFraud);
+        ResultAntiFraudService result = verifyFraud.verify(command);
         producer.send(result);
         acknowledgment.acknowledge();
         log.info("AntiFraud service worked with sucess, for correlationId {}, with status {}", result.correlationId(), result.riskFraud());
